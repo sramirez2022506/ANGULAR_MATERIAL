3eca69531d63b9d7bff0cad1e07d06e73f9e769c
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-09-12 12:22:28 -0600

    Se arreglo el datepicker de MMMM/DD/YYYY

--- src/app/datepicker-mdy/datepicker-mdy.component.ts
+++ src/app/datepicker-mdy/datepicker-mdy.component.ts
@@ -1,18 +1,44 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+
+import * as _moment from 'moment';
+
+import {default as _rollupMoment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-datepicker-mdy',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  providers: [provideMomentDateAdapter(MY_FORMATS)],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './datepicker-mdy.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './datepicker-mdy.component.css'
 })
 export class DatepickerMDYComponent {
-
+  readonly date = new FormControl(moment());
 }
